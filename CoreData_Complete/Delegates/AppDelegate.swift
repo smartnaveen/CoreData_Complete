0334e2012a91c837129907324c51b89ba7855c48
sean-radiofx <sean@alcax.com>
2025-08-15 21:23:23 +0530

    update appdelegate

--- CoreData_Complete/Delegates/AppDelegate.swift
+++ CoreData_Complete/Delegates/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
        print(FileManager.default.urls(for: .preferencePanesDirectory, in: .userDomainMask).first!)
+        debugPrint("Inside didFinishLaunchingWithOptions")
         return true
     }
 
