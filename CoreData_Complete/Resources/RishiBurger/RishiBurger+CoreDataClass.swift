cd825ee590cd7da5a4a474bf757533a21987f9bf
sean-radiofx <sean@alcax.com>
2025-08-15 12:29:55 +0530

    rishi burger model added

---
+++ CoreData_Complete/Resources/RishiBurger/RishiBurger+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  RishiBurger+CoreDataClass.swift
+//  CoreData_Complete
+//
+//  Created by Naveen Kumar on 15/08/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(RishiBurger)
+public class RishiBurger: NSManagedObject {
+
+}
---
+++ CoreData_Complete/Resources/RishiBurger/RishiBurger+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  RishiBurger+CoreDataProperties.swift
+//  CoreData_Complete
+//
+//  Created by Naveen Kumar on 15/08/25.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension RishiBurger {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<RishiBurger> {
+        return NSFetchRequest<RishiBurger>(entityName: "RishiBurger")
+    }
+
+    @NSManaged public var id: String?
+    @NSManaged public var product_name: String?
+
+}
+
+extension RishiBurger : Identifiable {
+
+}
