8c16eecb9d21d3415355ec512006e5d87e09198e
sean-radiofx <sean@alcax.com>
2025-08-15 11:10:32 +0530

    Added Delhi jal board model

---
+++ CoreData_Complete/Resources/DelhiJalBoard/DelhiJalBoard+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  DelhiJalBoard+CoreDataClass.swift
+//  CoreData_Complete
+//
+//  Created by Naveen Kumar on 15/08/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(DelhiJalBoard)
+public class DelhiJalBoard: NSManagedObject {
+
+}
